a2d6819e0d37cbbb60025848b8a513de79ad222f
dj04304 <dj043045@gmail.com>
2022-09-02 22:34:52 +0900

    디테일 페이지 수정 및 컨트롤러 기본설정

---
+++ src/main/resources/static/js/items-detail-page.js
@@ -0,0 +1,8 @@
+const imgSlide = document.querySelectorAll(".img-group img");
+const btnSlide = document.querySelector(".test-button");
+
+console.log(imgSlide[0]);
+console.log(imgSlide[1]);
+console.log(imgSlide[2]);
+console.log(btnSlide);
+
